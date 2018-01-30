f522704d8f797550d086b3543bdb371f7ed1d4cc
yakabay <yakabay86@gmail.com>
2018-01-30 20:53:24 +0200

    install api_platform core

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
