bb3ac5f56a8d6fbd121a0ec3cbe7d755bdc8b74c
Aleks Moshta <ahonymous@gmail.com>
2018-03-18 08:51:21 +0200

    added api doc tests

---
+++ tests/ApiDocControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ApiDocControllerTest.
+ */
+class ApiDocControllerTest extends WebTestCase
+{
+    public function testApiDoc()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'apidoc',
+            'PHP_AUTH_PW'   => 'qwerty',
+        ]);
+
+        $client->request(Request::METHOD_GET, '/api');
+
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertContains('udream', $client->getResponse()->getContent());
+    }
+
+    public function testWrongApiDoc()
+    {
+        $client = static::createClient();
+
+        $client->request(Request::METHOD_GET, '/api');
+
+        $this->assertSame(Response::HTTP_UNAUTHORIZED, $client->getResponse()->getStatusCode());
+    }
+}
