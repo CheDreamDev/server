c757914035a955d08161d3e5520256bd48320140
Aleks Moshta <ahonymous@gmail.com>
2018-03-26 20:29:04 +0300

    added softdeleteable

--- src/Entity/Faq/Faq.php
+++ src/Entity/Faq/Faq.php
@@ -4,6 +4,7 @@ namespace App\Entity\Faq;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -26,6 +27,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  *         "post"
  *     }
  * )
+ *
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt")
  */
 class Faq
 {
