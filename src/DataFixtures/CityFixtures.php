fc60588a39e65d9575b95aee25fc17c79886a90b
yakabay <yakabay86@gmail.com>
2018-03-29 02:32:31 +0300

    Add City entity, repository and fixtures

--- src/DataFixtures/CityFixtures.php
+++ src/DataFixtures/CityFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\City;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class CityFixtures extends Fixture
 {
     private $cities = [
         'Київ',
--- src/Entity/City.php
+++ src/Entity/City.php
@@ -7,11 +7,21 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\ExistsFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * @ORM\Table(name="cities")
  * @ORM\Entity(repositoryClass="App\Repository\CityRepository")
  *
- * @ApiResource()
+ * @ApiResource(
+ *     attributes={
+ *         "normalization_context"={"groups"={"read"}},
+ *         "denormalization_context"={"groups"={"write"}}
+ *     },
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get"}
+ * )
  * @ApiFilter(ExistsFilter::class, properties={"dreams"})
  */
 class City
@@ -20,16 +30,23 @@ class City
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     *
+     * @Groups("read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\Length(min=3, max=100)
+     * @Groups({"read", "write"})
      */
     private $name;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Dream", mappedBy="city")
+     *
+     * @Groups("read")
      */
     private $dreams;
 
@@ -47,14 +64,6 @@ class City
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id): void
-    {
-        $this->id = $id;
     }
 
     /**
