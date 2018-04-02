d4318d294695287e555b868a915eb78121c25277
Jack Cudnyi <evgeniychudniy@gmail.com>
2018-04-02 21:19:55 +0300

    Removed commented code, changed $birthday @var type

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -2,14 +2,8 @@
 
 namespace App\Entity\User;
 
-//use Doctrine\Common\Collections\ArrayCollection;
-//use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
-
-//use App\Entity\FinancialContribute;
-//use App\Entity\WorkContribute;
-//use App\Entity\OtherContribute;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
@@ -39,7 +33,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class User implements UserInterface, \Serializable
 {
 
-//    use ContactsInfo;
     use TimestampableEntity;
     use SoftDeleteableEntity;
 
@@ -135,48 +128,13 @@ class User implements UserInterface, \Serializable
      */
     protected $facebookId;
 
-//    /**
-//     * @ORM\ManyToMany(targetEntity="App\Entity\Dream", mappedBy="usersWhoFavorites" )
-//     */
-//    protected $favoriteDreams;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="App\Entity\FinancialContribute", mappedBy="user")
-//     */
-//    protected $financialContributions;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="App\Entity\Dream", mappedBy="author")
-//     */
-//    protected $dreams;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="App\Entity\EquipmentContribute", mappedBy="user")
-//     */
-//    protected $equipmentContributions;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="App\Entity\WorkContribute", mappedBy="user")
-//     */
-//    protected $workContributions;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="App\Entity\OtherContribute", mappedBy="user")
-//     */
-//    protected $otherContributions;
 
     /**
      * User constructor.
      */
     public function __construct()
     {
-//        $this->dreams = new ArrayCollection();
-//        $this->favoriteDreams = new ArrayCollection();
-//        $this->financialContributions = new ArrayCollection();
         $this->isActive = true;
-//        $this->equipmentContributions = new ArrayCollection();
-//        $this->workContributions = new ArrayCollection();
-//        $this->otherContributions = new ArrayCollection();
     }
 
     /**
@@ -320,7 +278,7 @@ class User implements UserInterface, \Serializable
     }
 
     /**
-     * @param mixed $birthday
+     * @param \DateTime $birthday
      *
      * @return User
      */
@@ -370,219 +328,4 @@ class User implements UserInterface, \Serializable
     {
         return $this->updatedAt;
     }
-
-//    /**
-//     * Add favoriteDreams
-//     *
-//     * @param  Dream $favoriteDreams
-//     *
-//     * @return User
-//     */
-//    public function addFavoriteDream(Dream $favoriteDreams)
-//    {
-//        $this->favoriteDreams[] = $favoriteDreams;
-//        $favoriteDreams->addUsersWhoFavorite($this);
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Remove favoriteDreams
-//     *
-//     * @param Dream $favoriteDreams
-//     */
-//    public function removeFavoriteDream(Dream $favoriteDreams)
-//    {
-//        $this->favoriteDreams->removeElement($favoriteDreams);
-//        $favoriteDreams->removeUsersWhoFavorite($this);
-//    }
-//
-//    /**
-//     * Get favoriteDreams
-//     *
-//     * @return \Doctrine\Common\Collections\Collection
-//     */
-//    public function getFavoriteDreams()
-//    {
-//        return $this->favoriteDreams;
-//    }
-//
-//    /**
-//     * Add dreams
-//     *
-//     * @param  Dream $dreams
-//     *
-//     * @return User
-//     */
-//    public function addDream(Dream $dreams)
-//    {
-//        $this->dreams[] = $dreams;
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Remove dreams
-//     *
-//     * @param Dream $dreams
-//     */
-//    public function removeDream(Dream $dreams)
-//    {
-//        $this->dreams->removeElement($dreams);
-//    }
-//
-//    /**
-//     * Get dreams
-//     *
-//     * @return Collection
-//     */
-//    public function getDreams()
-//    {
-//        return $this->dreams;
-//    }
-//
-//    /**
-//     * @param mixed $equipmentContributions
-//     */
-//    public function setEquipmentContributions($equipmentContributions)
-//    {
-//        $this->equipmentContributions = $equipmentContributions;
-//    }
-//
-//    /**
-//     * Add equipmentContributions
-//     *
-//     * @param  EquipmentContribute $equipmentContributions
-//     *
-//     * @return User
-//     */
-//    public function addEquipmentContribution(EquipmentContribute $equipmentContributions)
-//    {
-//        $this->equipmentContributions[] = $equipmentContributions;
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Remove equipmentContributions
-//     *
-//     * @param EquipmentContribute $equipmentContributions
-//     */
-//    public function removeEquipmentContribution(EquipmentContribute $equipmentContributions)
-//    {
-//        $this->equipmentContributions->removeElement($equipmentContributions);
-//    }
-//
-//    /**
-//     * Get equipmentContributions
-//     *
-//     * @return Collection
-//     */
-//    public function getEquipmentContributions()
-//    {
-//        return $this->equipmentContributions;
-//    }
-//
-//    /**
-//     * Get avatar
-//     *
-//     * @return string
-//     */
-//    public function getAvatar()
-//    {
-//        return '';
-//    }
-//
-//    /**
-//     * Add financialContributions
-//     *
-//     * @param  FinancialContribute $financialContributions
-//     *
-//     * @return User
-//     */
-//    public function addFinancialContribution(FinancialContribute $financialContributions)
-//    {
-//        $this->financialContributions[] = $financialContributions;
-//
-//        return $this;
-//    }
-//
-//
-//    /**
-//     * Remove financialContributions
-//     *
-//     * @param FinancialContribute $financialContributions
-//     */
-//    public function removeFinancialContribution(FinancialContribute $financialContributions)
-//    {
-//        $this->financialContributions->removeElement($financialContributions);
-//    }
-//
-//    /**
-//     * Get financialContributions
-//     *
-//     * @return \Doctrine\Common\Collections\Collection
-//     */
-//    public function getFinancialContributions()
-//    {
-//        return $this->financialContributions;
-//    }
-//
-//    /**
-//     * Add workContributions
-//     *
-//     * @param  WorkContribute $workContributions
-//     *
-//     * @return User
-//     */
-//    public function addWorkContribution(WorkContribute $workContributions)
-//    {
-//        $this->workContributions[] = $workContributions;
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Remove workContributions
-//     *
-//     * @param WorkContribute $workContributions
-//     */
-//    public function removeWorkContribution(WorkContribute $workContributions)
-//    {
-//        $this->workContributions->removeElement($workContributions);
-//    }
-//
-//    /**
-//     * Get workContributions
-//     *
-//     * @return \Doctrine\Common\Collections\Collection
-//     */
-//    public function getWorkContributions()
-//    {
-//        return $this->workContributions;
-//    }
-//
-//    /**
-//     * Add otherContributions
-//     *
-//     * @param  OtherContribute $otherContributions
-//     *
-//     * @return User
-//     */
-//    public function addOtherContribution(OtherContribute $otherContributions)
-//    {
-//        $this->otherContributions[] = $otherContributions;
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * Remove otherContributions
-//     *
-//     * @param OtherContribute $otherContributions
-//     */
-//    public function removeOtherContribution(OtherContribute $otherContributions)
-//    {
-//        $this->otherContributions->removeElement($otherContributions);
-//    }
 }
